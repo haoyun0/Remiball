c67da90c06c34d1c1f0ce89222f7c1088232652d
haoyun0 <847360401@qq.com>
2022-05-15 20:11:20 +0800

    gb_control 部分send新增返回了消息id(int) echo部分新增指令Rick，可输入参数生成虚假视频分享链接

--- awesome_bot/plugins/_gb/gb_control.py
+++ awesome_bot/plugins/_gb/gb_control.py
@@ -1,4 +1,5 @@
 from .gb_database import DataList
+import codecs
 
 class Con:
     def __init__(self):
@@ -33,7 +34,20 @@ class Con:
             message = "[CQ:at,qq=%d] " % event.user_id + message
         #message = "DEBUG:\nmessage_type:%s\nsub_type:%s\nmsgid:%s\n" % (event.message_type, event.sub_type, event.message_id) + message
         msgid = await bot.call_api("send_msg", message_type=event.message_type, user_id=event.user_id, group_id=gid, message=message, auto_escape=auto_escape)
-        return msgid['message_id']
+        msgid = str(msgid['message_id']).encode()
+        msgid = codecs.decode(msgid, 'base64')
+        if event.message_type == 'group':
+            gid = self.ByteToInt(msgid[0: 4])
+            uid = self.ByteToInt(msgid[4: 8])
+            seqid = self.ByteToInt(msgid[8: 12])
+            timestamp = self.ByteToInt(msgid[16: 20])
+        elif event.message_type == 'private':
+            uid = self.ByteToInt(msgid[0: 4])
+            seqid = self.ByteToInt(msgid[4: 8])
+            timestamp = self.ByteToInt(msgid[12: 16])
+        else:
+            seqid = 0
+        return seqid
 
     def get_gid(self, event):
         if event.message_type == 'group':
@@ -43,5 +57,7 @@ class Con:
                 return '19260817'  # 好友消息
             else:
                 return '60481729'  # 群临时会话
+    def ByteToInt(self, s:bytes):
+        return s[3] + 256 * (s[2] + 256 * (s[1] + 256 * s[0]))
 
 con = Con()
--- awesome_bot/plugins/echo/__init__.py
+++ awesome_bot/plugins/echo/__init__.py
@@ -1,21 +1,77 @@
 from .._gb import *
 
 
-echo0 = on_command('echo', aliases={'echo'}, permission=SUPERUSER, rule=Check_('echo'), priority=1)
-echo1 = on_command('test', permission=SUPERUSER, rule=Check_('test'), priority=1)
+echo0 = on_command('echo', permission=SUPERUSER, rule=Check_('echo'), priority=1)
+echo1 = on_command('send', permission=SUPERUSER, rule=Check_('send'), priority=1)
+echo2 = on_command('test', rule=Check_('test', 'group'), permission=SUPERUSER, priority=1)
+echo_rick = on_command('rick', rule=Check_('rick', 'group'), priority=10)
 
 cmds = [
 'echo',
-'test'
+'send',
+'test',
+'rick'
 ]
 con.addcmds('基础功能', cmds)
 
 @echo0.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
-    await con.send(bot, event, str(event.get_message()))
+    mid = await con.send(bot, event, str(event.get_message()))
+    if mid:
+        await con.send(bot, event, 'message_id = ' + str(mid))
     await echo0.finish()
 
 @echo1.handle()
 async def handle(bot: Bot, event: Event, state: T_State):
-    await con.send(bot, event, str(event.get_message()), auto_escape=True)
+    mid = await con.send(bot, event, str(event.get_message()), auto_escape=True)
+    if mid:
+        await con.send(bot, event, 'message_id = ' + str(mid))
     await echo1.finish()
+
+@echo2.handle()
+async def handle(bot: Bot, event: Event, state: T_State):
+    msg = str(event.get_message()).strip().split()
+
+    msgs = []
+    # msgs.append(segement('text', text='这是一条测试信息\n'))
+    # msgs.append(segement('face', id='123'))
+    # msgs.append(segement('at', qq='847360401'))
+    # msgs.append(segement('image', file='https://pics3.baidu.com/feed/5882b2b7d0a20cf4ce4aaa5d988e6830aeaf99ec.png'))
+    msgs.append(segement('image', file=r'C:\Database\image\xp\0xa0e66ae7d673fc3e.jpg'))
+    msgid = await bot.call_api('send_group_msg', message=msgs, group_id=event.group_id)
+    #msgid = await con.send(bot, event, msgs)
+    await con.send(bot, event, 'msg_id = %s' % msgid['message_id'])
+    msg = await bot.call_api('get_msg', message_id=msgid['message_id'])
+    await con.send(bot, event, 'get_msg = ' + str(msg))
+    await echo2.finish()
+
+@echo_rick.handle()
+async def handle(bot: Bot, event: Event, state: T_State):
+    msg = str(event.get_message()).strip()
+    if '[CQ:image,' in msg:
+        left = msg.index('[CQ:image,')
+        right = msg.index(']', left + 5)
+        urlid = msg.index(',url=', left + 5, right)
+        url = msg[urlid + 5:right]
+        msg = msg[:left] + msg[right+1:]
+    else:
+        url = 'https://gchat.qpic.cn/gchatpic_new/323690346/963370810-2433807771-5B8FC6AFD2A02A7124A6550C64ACF8C2/0?term=2'
+
+    msg = msg.split()
+    msgs = []
+    if len(msg) >=1:
+        title = msg[0]
+    else:
+        title = "球图"
+    if len(msg) >=2:
+        content = msg[1]
+    else:
+        content = "分享一些可爱的白泽球图"
+    msgs.append(segement('share',
+                         title=title,
+                         content=content,
+                         url="www.bilibili.com/video/BV1uT4y1P7CX",
+                         image=url)
+                )
+    await bot.call_api('send_group_msg', message=msgs, group_id=event.group_id)
+    await echo_rick.finish()
